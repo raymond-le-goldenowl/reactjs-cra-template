c6125f6f830c422c89f3d1dccfa917282f573c3d
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 06:57:37 +0700

    remoev test.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-      import './App.css'
+import './App.css'
 
-function  App() {
+function App() {
   return (
-    <div  className="App">
-                <header className="App-header">
-              <p>
+    <div className="App">
+      <header className="App-header">
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-          </a>
+          Learn React
+        </a>
       </header>
     </div>
   )

d7a17d5528fa9a5f67f89910ace9f95fa095f28d
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 14:18:04 +0700

    Fix error syntax

--- src/ClassComponents/EditTodoModel.jsx
+++ src/ClassComponents/EditTodoModel.jsx
@@ -6,16 +6,16 @@ export default class EditTodoModel extends Component {
       <>
         {/* Modal */}
         <div
-          class="modal fade"
+          className="modal fade"
           id="EditTodoModel"
-          tabindex="-1"
+          tabIndex="-1"
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
                   {this.props?.todo?.id ? (
                     <span>Edit todo with id = {this.props?.todo?.id}</span>
                   ) : (
@@ -24,24 +24,24 @@ export default class EditTodoModel extends Component {
                 </h5>
                 <button
                   type="button"
-                  class="btn-close"
+                  className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
               {/* input to update todo */}
-              <div class="modal-body">
+              <div className="modal-body">
                 <input type="text" />
               </div>
-              <div class="modal-footer">
+              <div className="modal-footer">
                 <button
                   type="button"
-                  class="btn btn-secondary"
+                  className="btn btn-secondary"
                   data-bs-dismiss="modal"
                 >
                   Close
                 </button>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                   Save changes
                 </button>
               </div>

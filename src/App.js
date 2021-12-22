13fc4a25459a00de936acfe0870eccc2fe58f380
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 18:15:15 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
-import MainTodo from './ClassComponents/TodoListApp/MainTodo'
+// import MainTodo from './ClassComponents/TodoListApp/MainTodo'
+import Counter from './FunctionalComponents/useState/Counter'
 
 function App() {
   return (
     <div className="App">
-      <MainTodo />
+      {/* <MainTodo /> */}
+      <Counter />
     </div>
   )
 }
---
+++ src/FunctionalComponents/useState/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+  const [count, setCount] = useState(() => 0)
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: 30 }}>
+      <button
+        style={{ fontSize: '4rem', padding: '3rem' }}
+        onClick={() => setCount(prev => prev + 1)}
+      >
+        +
+      </button>
+      <span
+        style={{ fontSize: '4rem', padding: '3rem' }}
+        onClick={() => setCount(prev => prev + 1)}
+      >
+        {count}
+      </span>
+      <button
+        style={{ fontSize: '4rem', padding: '3rem' }}
+        onClick={() => setCount(prev => prev - 1)}
+      >
+        -
+      </button>
+    </div>
+  )
+}

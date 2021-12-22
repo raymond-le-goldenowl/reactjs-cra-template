0664ea7094d5291b37264ba46c15995f7b4f7573
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 11:16:19 +0700

    Add checkbox

--- src/ClassComponents/Todo.jsx
+++ src/ClassComponents/Todo.jsx
@@ -6,6 +6,12 @@ export default class Todo extends Component {
       <div>
         <h4 style={{ fontSize: '1.6rem' }}>
           {this.props.todo.content}
+          <input
+            type="checkbox"
+            name="is-done"
+            id="is-done"
+            style={{ cursor: 'pointer' }}
+          />
           <button>Delete</button>
           <button>Edit</button>
         </h4>

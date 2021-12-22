28fe6706fcc071cbca28efc60ddf9bf51991ea4a
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 13:43:54 +0700

    Remove file link.js

--- src/links.js
+++
@@ -1,4 +0,0 @@
-export const links = [
-  { id: 1, link: '/', title: 'home', icon: "<span class='material-icons'>home</span>" },
-  { id: 1, link: '/', title: 'home', icon: "<span class='material-icons'>contact_support</span>" }
-]

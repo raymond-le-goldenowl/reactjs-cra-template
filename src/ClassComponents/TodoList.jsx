d3cff5187752c3a177dd1ed78fee9de61fcca0a7
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 10:45:19 +0700

    Craete TodoList.jsx

---
+++ src/ClassComponents/TodoList.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Todo from './Todo'
+
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { todo_list: props.todo_list || [] }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.todo_list.map((todo, index) => (
+          <Todo key={index} todo={todo} />
+        ))}
+      </div>
+    )
+  }
+}

cdd7e11a63427c78443c0275ac7d126cbbcef4f5
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-22 08:01:59 +0700

    Add links

--- src/links.js
+++ src/links.js
@@ -1,4 +1,4 @@
 export const links = [
   { id: 1, link: '/', title: 'home', icon: "<span class='material-icons'>home</span>" },
-  { id: 1, link: '/', title: 'home', icon: "<span class='material-icons'>contact_support</span>" },
-];
+  { id: 1, link: '/', title: 'home', icon: "<span class='material-icons'>contact_support</span>" }
+]

9e155892662c186fdcbdc4934b1d6934040c89b5
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-23 14:26:12 +0700

    Functional Component to Class Component

--- src/ClassComponents/LoopAndDisplayData/RenderListUsers.js
+++ src/ClassComponents/LoopAndDisplayData/RenderListUsers.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import users from './users-data'
-
-export default function RenderListUsers() {
-  console.log(users)
-
-  return (
-    <div>
-      <ul>
-        {users.map(user => (
-          <li>
-            {user.name} | {user.location} | {user.car}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+export default class RenderListUsers extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <ul>
+          {users.map(user => (
+            <li>
+              {user.name} | {user.location} | {user.car}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }

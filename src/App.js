bbaa0d2d1405c5e8bc19ac03e44c13f0a18d4148
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-25 09:25:47 +0700

    Babel module resolver, routerv6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React, { lazy } from 'react'
+import { Route, Switch, REdirect } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
 import './App.css'
-import MainTodo from './ClassComponents/TodoListApp/MainTodo'
+// import About from './About'
+// import MainTodo from './ClassComponents/TodoListApp/MainTodo'
+
+const About = lazy(() => import('./About'))
+const MainTodo = lazy(() => import('./ClassComponents/TodoListApp/MainTodo'))
 
 function App() {
   console.log(process.env.REACT_APP_CLIENT_ID)
@@ -8,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <MainTodo />
+      <About />
     </div>
   )
 }

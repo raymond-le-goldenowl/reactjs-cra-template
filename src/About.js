971ae86a807161c671cd142aaaeed36a60d16036
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-25 09:26:21 +0700

    add about file

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>About page</h1>
+      </div>
+    )
+  }
+}

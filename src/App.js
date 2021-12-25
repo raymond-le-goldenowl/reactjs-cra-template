75482ab080afd1672c39c738f605d9b0607894ba
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2021-12-25 14:31:46 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, { lazy } from 'react'
-import { Route, Switch, REdirect } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
-
+import React, { Suspense } from 'react'
+import { BrowserRouter, NavLink } from 'react-router-dom'
+import { history, renderRouteConfigs, _routes } from './Router'
 import './App.css'
-// import About from './About'
-// import MainTodo from './ClassComponents/TodoListApp/MainTodo'
-
-const About = lazy(() => import('./About'))
-const MainTodo = lazy(() => import('./ClassComponents/TodoListApp/MainTodo'))
 
 function App() {
   console.log(process.env.REACT_APP_CLIENT_ID)
   console.log(process.env.REACT_APP_KEY)
 
   return (
-    <div className="App">
-      <MainTodo />
-      <About />
-    </div>
+    <>
+      <div className="App">
+        <Suspense fallback={<p>Loading...</p>}>
+          <BrowserRouter history={history}>
+            <nav>
+              <ul className="nav">
+                <li className="nav-item">
+                  <NavLink to={'/'}>Main toso list</NavLink>
+                </li>
+
+                <li className="nav-item">
+                  <NavLink to={'/about'}>About</NavLink>
+                </li>
+              </ul>
+            </nav>
+            {renderRouteConfigs(_routes)}
+          </BrowserRouter>
+        </Suspense>
+        <h1>Xin chao</h1>
+      </div>
+    </>
   )
 }
 
